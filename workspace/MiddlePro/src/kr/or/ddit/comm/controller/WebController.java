ce40028d5d2ef75ff19cbba6d4f9e4c2151d9fcd
eubnaeuu <cdwcdw34@gmail.com>
2021-04-21 09:56:21 +0900

    2021-04-21  9:56:20.87

--- workspace/MiddlePro/src/kr/or/ddit/comm/controller/WebController.java
+++ workspace/MiddlePro/src/kr/or/ddit/comm/controller/WebController.java
@@ -78,8 +78,6 @@ public class WebController extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		process(req, resp);
 	}
-
-	
 	/**
 	 * 요청 처리 메서드
 	 * @param req
@@ -135,20 +133,5 @@ public class WebController extends HttpServlet {
 				dispatcher.forward(req, resp);
 			}
 		}
-		
-	
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 	}
 }
