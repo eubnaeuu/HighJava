eef89ba9411b4d9f877f01648b4e1905dade3739
eubnaeuu <cdwcdw34@gmail.com>
2021-04-23 10:56:21 +0900

    2021-04-23 10:56:20.68

--- workspace/MiddlePro/src/kr/or/ddit/friendreq/handler/UpdateFriendReqHandler.java
+++ workspace/MiddlePro/src/kr/or/ddit/friendreq/handler/UpdateFriendReqHandler.java
@@ -4,6 +4,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kr.or.ddit.comm.handler.CommandHandler;
+import kr.or.ddit.friend.service.FriendService;
+import kr.or.ddit.friend.service.FriendServiceImpl;
+import kr.or.ddit.friend.vo.FriendVO;
 import kr.or.ddit.friendreq.service.FriendReqService;
 import kr.or.ddit.friendreq.service.FriendReqServiceImpl;
 import kr.or.ddit.friendreq.vo.FriendReqVO;
@@ -22,22 +25,29 @@ public class UpdateFriendReqHandler implements CommandHandler {
 		
 //		System.out.println("입장 FriendReq Main Haldler 입장");
 		
-		
-		// 
-		FriendReqService FriendReqService = FriendReqServiceImpl.getInstance();
+		FriendReqService friendReqService = FriendReqServiceImpl.getInstance();
 		
 			System.out.println("update중update중update중update중update중update중update중");
 			
-			FriendReqVO fv = new FriendReqVO();
+			FriendReqVO frv = new FriendReqVO();
+			frv.setFriendId(req.getParameter("friendId"));
+			frv.setMemId(req.getParameter("memId"));
+			frv.setReqYn(req.getParameter("reqYn"));
+			
+			
+			// 일촌목록 추가
+			FriendVO fv = new FriendVO();
+			FriendService friendService = FriendServiceImpl.getInstance();
+			
 			fv.setFriendId(req.getParameter("friendId"));
 			fv.setMemId(req.getParameter("memId"));
-			fv.setReqYn(req.getParameter("reqYn"));
 			
-			int cnt = FriendReqService.updateFriendReq(fv);
+			int cnt = friendReqService.updateFriendReq(frv);
+			int cnt2 = friendService.insertFriend(fv);
 			
 			String msg = "";
 			
-			if(cnt > 0) {
+			if(cnt > 0 && cnt2 > 0) {
 				msg = "성공";
 			}else {
 				msg = "실패";
